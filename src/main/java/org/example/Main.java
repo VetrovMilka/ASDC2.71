0fa469c8ff0d1a2d6719f3ee7f6ff9af716f0391
VetrovMilka <VetrovMilka@github.com>
2023-02-23 19:16:43 +0200

    Small refactor in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.util.List;
 
 public class Main {
+    private static final String FILE_PATH = "Products.csv";
     public static void main(String[] args) throws IOException {
 
         Product product = new Product(27, "Макароны", "Макароны", 50, 122, "piece");
-        Service.writeData("Products.csv", product);
-        List<Product> products = Service.readData("Products.csv");
+        Service.writeData(FILE_PATH, product);
+
+        List<Product> products = Service.readData(FILE_PATH);
         products.forEach(System.out::println);
     }
 
